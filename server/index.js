71f08cc90edb75d715e61c24e3cf2551d0d25303
Chloe Tan <chloe.tan95@gmail.com>
2021-03-04 00:13:58 -0500

    Connected to db. Stuck on querying all titles in db

--- server/index.js
+++ server/index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const clientDist = path.join(__dirname, '/../client/dist');
+const dbHelper = require(__dirname + '/../database/config.js');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(`${clientDist}`));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.get('/get', (req, res) => {
+  console.log('hello');
+  dbHelper.createTitle(
+    {
+      titleId: 1,
+      listingID: 10,
+      listingName: 'Spaceship',
+      listingLocation: 'Mars'
+    });
+
+  dbHelper.findAllTitles()
+    .catch((err) => {
+      console.err('findAllTitles err: ', err);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
