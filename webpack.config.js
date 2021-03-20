a090cc164231a4596e831b7fa5355c8159f6d4e4
Chloe Tan <chloe.tan95@gmail.com>
2021-03-20 16:56:13 -0400

    Moved index.html outside dist folder so that it does not get ignored. Also added config to have webpack make a copy of index.html in the dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
 const path = require('path');
 const srcPath = path.join(__dirname, '/client/src');
 const distPath = path.join(__dirname, '/client/dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  watch: true,
   entry: `${srcPath}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: distPath
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
-        include: srcPath,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/react', '@babel/env']
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react', '@babel/env']
+          }
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html'
+    })
+  ],
 };
